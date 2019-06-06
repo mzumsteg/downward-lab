e914877c72f4b673f1fce731f0473ed73a0a9ae0
Martin Zumsteg <mar.zumsteg@stud.unibas.ch>
2019-06-06 16:51:27 +0200

    Added support for txt/csv format to domain evaluators

--- code/domain_comparison_report.py
+++ code/domain_comparison_report.py
@@ -7,6 +7,7 @@ from math import sqrt
 from downward.reports import PlanningReport
 
 class OptimalStrategyEvaluator:
+    OUTPUT_FORMATS = "tex txt".split()
     """
     Creates a table listing how often a strategy is optimal for each group.
     """
@@ -16,6 +17,8 @@ class OptimalStrategyEvaluator:
     
     def setReport(self, report):
         self.report = report
+        if report.output_format not in self.OUTPUT_FORMATS:
+            raise ValueError('invalid format: {}'.format(report.output_format))
         if len(report.attributes) != 1:
             raise ValueError("Evaluator needs exactly one attribute")
         self.attribute = report.attributes[0]
@@ -34,7 +37,7 @@ class OptimalStrategyEvaluator:
         else:
             return r"\textbf{0}"
     
-    def format(self, groups):
+    def _format_tex(self, groups):
         if len(groups) > 0:
             lines = []
             lines.append(r"\begin{center}\begin{tabular}{@{}l" + "|c" * len(self.report.algorithm_names) + "@{}}")
@@ -65,9 +68,31 @@ class OptimalStrategyEvaluator:
             return "\n".join(lines)
         else:
             return r"\textbf{NO DATA}"
+    
+    def _format_txt(self, groups):
+        lines = [",".join(["domain", "count"] + self.report.algorithm_names)]
+        if len(groups) > 0:
+            for group, problems in groups.items():
+                optimals = []
+                for problem, algorithms in problems.items():
+                    optimals.append(self._get_optimal(list(map(lambda run: run[self.attribute], algorithms))))
+                line = ["{:.7f}".format(sum(x) / len(x)) for x in zip(*optimals)]
+                lines.append(",".join([group, str(len(problems))] + line))
+            optimals = []
+            for group, problems in groups.items():
+                for problem, algorithms in problems.items():
+                    optimals.append(self._get_optimal(list(map(lambda run: run[self.attribute], algorithms))))
+            line = ["{:.7f}".format(sum(x) / len(x)) for x in zip(*optimals)]
+            lines.append(",".join(["_total", str(sum(map(len, groups.values())))] + line))
+        return "\n".join(lines)
+    
+    def format(self, groups):
+        return getattr(self, "_format_" + self.report.output_format)(groups)
 
-PARAM_PATTERN = r"$\mathbb{{V}} = \left[{}, {}\right];\; \mu = {:.1f};\; \sigma = {:.2f}$"
 class ProblemStatisticsEvaluator:
+    TEX_CELL_PATTERN = r"$\mathbb{{V}} = \left[{}, {}\right];\; \mu = {:.1f};\; \sigma = {:.2f}$"
+    TXT_CELL_PATTERN = "{},{},{:.3f},{:.5f}"
+    OUTPUT_FORMATS = "tex txt".split()
     """
     Creates a table listing statistical attributes of each domain
     (generated over it's problems).
@@ -77,12 +102,14 @@ class ProblemStatisticsEvaluator:
     
     def setReport(self, report):
         self.report = report
+        if report.output_format not in self.OUTPUT_FORMATS:
+            raise ValueError('invalid format: {}'.format(report.output_format))
         # the planner is never run without an algorithm,
         # so to avoid duplicate data we have to pick one still
         if len(report.algorithm_names) != 1:
             raise ValueError("Evaluator needs exactly one algorithm")
     
-    def format(self, groups):
+    def _format_tex(self, groups):
         if len(groups) > 0:
             lines = []
             lines.append(r"\begin{center}\begin{tabular}{@{}l" + "|c" * len(self.report.attributes) + "@{}}")
@@ -97,7 +124,7 @@ class ProblemStatisticsEvaluator:
                     values = list(map(lambda run: run[0][attrib], problems.values()))
                     lower, upper, mean = min(values), max(values), sum(values) / len(values)
                     stddev = sqrt(sum(map(lambda x: (x - mean)**2, values)) / (len(values) - 1))
-                    line.append(PARAM_PATTERN.format(lower, upper, mean, stddev))
+                    line.append(self.TEX_CELL_PATTERN.format(lower, upper, mean, stddev))
                 lines.append(" & ".join(line) + r"\\")
             # emit totals
             lines.append(r"\midrule")
@@ -108,12 +135,44 @@ class ProblemStatisticsEvaluator:
                     values += list(map(lambda run: run[0][attrib], problems.values()))
                     lower, upper, mean = min(values), max(values), sum(values) / len(values)
                     stddev = sqrt(sum(map(lambda x: (x - mean)**2, values)) / (len(values) - 1))
-                line.append(PARAM_PATTERN.format(lower, upper, len(values), mean, stddev))
+                line.append(self.TEX_CELL_PATTERN.format(lower, upper, len(values), mean, stddev))
             lines.append(" & ".join(line))
             lines.append(r"\end{tabular}\end{center}")
             return "\n".join(lines)
         else:
             return r"\textbf{NO DATA}"
+    
+    def _format_txt(self, groups):
+        lines = []
+        line = ["domain", "count"]
+        for attrib in self.report.attributes:
+            line.append("min_" + attrib)
+            line.append("max_" + attrib)
+            line.append("mean_" + attrib)
+            line.append("std_" + attrib)
+        lines.append(",".join(line))
+        if len(groups) > 0:
+            for group, problems in groups.items():
+                line = [group, str(len(problems))]
+                for attrib in self.report.attributes:
+                    values = list(map(lambda run: run[0][attrib], problems.values()))
+                    lower, upper, mean = min(values), max(values), sum(values) / len(values)
+                    stddev = sqrt(sum(map(lambda x: (x - mean)**2, values)) / (len(values) - 1))
+                    line.append(self.TXT_CELL_PATTERN.format(lower, upper, mean, stddev))
+                lines.append(",".join(line))
+            line = ["_total", str(sum(map(len, groups.values())))]
+            for attrib in self.report.attributes:
+                values = []
+                for group, problems in groups.items():
+                    values += list(map(lambda run: run[0][attrib], problems.values()))
+                    lower, upper, mean = min(values), max(values), sum(values) / len(values)
+                    stddev = sqrt(sum(map(lambda x: (x - mean)**2, values)) / (len(values) - 1))
+                line.append(self.TXT_CELL_PATTERN.format(lower, upper, len(values), mean, stddev))
+            lines.append(",".join(line))
+        return "\n".join(lines)
+    
+    def format(self, groups):
+        return getattr(self, "_format_" + self.report.output_format)(groups)
 
 class DomainComparisonReport(PlanningReport):
     """
